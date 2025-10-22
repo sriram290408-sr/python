a3ff10fb2d38da3d8b38d0dda5a32f0b4748e575
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-10-22 14:53:58 +0530

    update

--- 22.10.2025/main.py
+++ 22.10.2025/main.py
@@ -1,58 +1,58 @@
-# #Question - 1
-# #(i)
-# Sentence = str(input("Enter the Sentence: "))
+#Question - 1
+#(i)
+Sentence = str(input("Enter the Sentence: "))
 
-# #Split
-# word = Sentence.split('-')
+#Split
+word = Sentence.split('-')
 
-# print(word)
+print(word)
 
-# #(ii)
-# sentence = "Emma-is-a-data-scientist"
+#(ii)
+sentence = "Emma-is-a-data-scientist"
 
-# word = ""
-# for alpha in sentence:
-#     if alpha != '-':           
-#         word += alpha
-#     else:
-#         print(word)           
-#         word = ""    
+word = ""
+for alpha in sentence:
+    if alpha != '-':           
+        word += alpha
+    else:
+        print(word)           
+        word = ""    
 
-# print(word)              
+print(word)              
 
-# #Question - 2
-# #(i)
-# sentence = str(input("Enter the Sentence: "))
+#Question - 2
+#(i)
+sentence = str(input("Enter the Sentence: "))
 
-# word = sentence[::-1]
-# print(word)
+word = sentence[::-1]
+print(word)
 
-# #Question - 3
-# sentence = str(input("Enter the Sentence:"))
-# count = 0
+#Question - 3
+sentence = str(input("Enter the Sentence:"))
+count = 0
 
-# for i in range(0, len(sentence), +1):
-#     if sentence[i] not in ['a', 'e', 'i', 'o', 'u', " "]:
-#         count += 1
+for i in range(0, len(sentence), +1):
+    if sentence[i] not in ['a', 'e', 'i', 'o', 'u', " "]:
+        count += 1
 
-# print(count)
+print(count)
 
-#Question - 4
-# sentence = str(input("Enter the Sentence: "))
-# word = " "
+# Question - 4
+sentence = str(input("Enter the Sentence: "))
+word = " "
 
-# for i in sentence:
-#     if i != " ":
-#         word += i
-# print(word) 
+for i in sentence:
+    if i != " ":
+        word += i
+print(word) 
 
-# # Question - 5
-# password = str(input("Enter the Password: "))
-# special = "!@#$%^&*"
+# Question - 5
+password = str(input("Enter the Password: "))
+special = "!@#$%^&*"
 
-# for i in password:
-#     if i in special:
-#         print("Password is strong")
-#         break
-# else:
-#     print("Password is not strong")
+for i in password:
+    if i in special:
+        print("Password is strong")
+        break
+else:
+    print("Password is not strong")
