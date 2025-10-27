03167e3ec4804571b00b7d8aa4494ceb19239218
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-10-27 15:08:31 +0530

    new commit

---
+++ python_module/math.py
@@ -0,0 +1,17 @@
+# import math
+
+# x = 5
+# #function
+# result = math.factorial(5)
+# print(result)
+
+# y = 49
+# result = math.sqrt(y)
+# print(int(result))
+
+# print(int(math.sin(math.radians(90))))
+
+# content = "hello world"
+# print(content.capitalize())
+# print(content.casefold())
+# print(content.swapcase())
