38de85e015c2c6d67d03f03b413510f5e169006c
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-11-20 15:00:51 +0530

    new practice

--- 20.11.25/main.py
+++ 20.11.25/main.py
@@ -32,3 +32,55 @@ for k in range(a+1, b-1 + 1):
     result.append(k)
 
 print(result)
+
+#sum-3
+given = "Anna went to America and met Adam"
+given2 = given.lower()
+count = 0
+new1 = [given2[0]]
+new2 = []
+
+for i in range(1,len(given2),+1):
+    if given2[i] == " ":
+        new1.append(given2[i+1])
+        new2.append(given2[i-1])
+        
+for j in range(0,len(new1)-1,+1):
+    if new1[j] == new2[j]:
+        count += 1
+        
+print(count)
+
+#sum-4
+# Print the following pattern using loops:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+n = 5
+start = 1
+result = ""
+while start <= n:
+    result += str(start)
+    print(result)
+    start += 1
+
+#sum-4
+n = 1
+start = 5
+result = ""
+while start >= n:
+    result += str(start)
+    print(result)
+    start -= 1
+
+#sum-5
+n = 1
+result = ""
+start = 5
+while start >= n:
+    result = result + str(start) + " "
+    print(" " * (start - n) + result)
+    start -= 1
